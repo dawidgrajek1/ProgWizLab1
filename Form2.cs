05c04c90feea238a4f23e7ef15f7ea78e6434ea9
rertis <kabiron@o2.pl>
2023-03-07 11:02:26 +0100

    Dodano stoper

--- Form2.cs
+++ Form2.cs
@@ -12,14 +12,40 @@ namespace pierwszyproj2
 {
     public partial class Form2 : Form
     {
+        TimeSpan timePassed = TimeSpan.Zero;
+
         public Form2()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void Form2_Load(object sender, EventArgs e)
         {
+            label1.Text = "00:00:00";
+            timer1.Interval = 1000;
+        }
 
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = timePassed.ToString();
+            timePassed = timePassed.Add(new TimeSpan(0,0,1));
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            label1.Text = "00:00:00";
+            timePassed = TimeSpan.Zero;
         }
     }
 }
